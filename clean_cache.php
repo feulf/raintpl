5651591cc149246d6d39220061e73c6d88037d28
Rain <rainelemental@gmail.com>
2012-02-11 13:00:02 -0500

    Fixed clean cache

--- clean_cache.php
+++ clean_cache.php
@@ -4,6 +4,6 @@
 	include "inc/rain.tpl.class.php";
 
 	// clean all the compiled templates from cache
-	raintpl::clean_cache();
+	array_map( "unlink", glob( raintpl::$cache_dir . "*.rtpl.php" ) );
         
 ?>
